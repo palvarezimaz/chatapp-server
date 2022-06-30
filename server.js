17c32f6f961fcdb5f5626fad28aa86a8a8c5fa29
Ikilzig <palvarezimaz@gmail.com>
2022-06-30 10:03:34 +1000

    get line

--- server.js
+++ server.js
@@ -10,8 +10,19 @@ const io = socketIo(server, {
   },
 }); //in case server and client run on different urls
 
-/// Chat rooms
+app.get('/', (req, res) => {
+  res.send(
+    '<h1>ChatApp Server - Thanks for putting me up!</h1><br><h3>Access the client via port 3000</h3>'
+  );
+});
+// Message const
+const NewMessageEvent = 'new-message-event';
+//  Chat rooms
 const chatRooms = ['General', 'Jokies', 'clock-room'];
+
+// app.get('/getRooms', (req, res) => {
+//   res.json({ rooms: chatRooms });
+// });
 
 io.on('connection', (socket) => {
   console.log('client connected: ', socket.id);
@@ -19,12 +30,27 @@ io.on('connection', (socket) => {
   // socket.join('clock-room');
   socket.join(chatRooms[0]);
   ////
-
+  // io.on('connection', (socket) => {
+  //   socket.on('chat message', (msg) => {
+  //     console.log('message: ' + msg);
+  //   });
+  // });
+  // io.on('connection', (socket) => {
+  //   socket.on('chat message', (msg) => {
+  //     io.emit('chat message', msg);
+  //   });
+  // });
+  /// Sending messages
+  socket.on(NewMessageEvent, (data) => {
+    io.in(chatRooms[0]).emit(NewMessageEvent, data);
+  });
   /////// emit to just connected
   // socket.emit('connection', null)
 
   socket.on('disconnect', (reason) => {
     console.log(reason);
+    console.log('user disconnected');
+    socket.leave(chatRooms[0]);
   });
 });
 
