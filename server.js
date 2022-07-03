331e3ad94530793b8269fc7495e92e3649bda309
Ikilzig <palvarezimaz@gmail.com>
2022-07-03 15:59:40 +1000

    server update

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 }
