79e08444eeedf1d98ee8505edb6e33088ad0d344
palvarzimaz <palvarezimaz@gmail.com>
2023-02-11 19:59:20 +1100

    cleaning comments - no refactoring

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const http = require('http');
 const PORT = process.env.PORT || 3002;
 const app = express();
 const server = http.createServer(app);
+
 let origin = 'http://localhost:3000';
+
 if (process.env.NODE_ENV === 'production') {
   origin = 'https://paichatapp.herokuapp.com/';
 }
@@ -14,16 +16,8 @@ const io = socketIo(server, {
   },
 });
 
-///Server chat rooms ////// Future Development
-// const chatRooms = ['General', 'Jokies', 'clock-room'];
 
-// app.get('/', (req, res) => {
-//   res.send(
-//     '<h1>ChatApp Server - Thanks for putting me up!</h1><br><h3>Access the client via port 3000</h3>'
-//   );
-// });
-
-// User name - HANDSHAKE -- NEEDS WORK
+// User name - HANDSHAKE
 io.use((socket, next) => {
   const userN = socket.handshake.auth.user;
   socket.userName = userN;
@@ -42,35 +36,15 @@ io.on('connection', (socket) => {
   }
 
   io.emit('users', users);
-
-  // notify existing users
-  // socket.broadcast.emit('user connected', {
-  //   userID: socket.id,
-  //   username: socket.userName,
-  // });
-
   console.log('a user connected');
   console.log(`Socket id: ${socket.id}`);
 
-  // socket.join(chatRooms[0]);
-
-  /// Unique welcome message - FUTURE DEVELOPMENT
-  // socket.on('Welcome Message', (name) => {
-  // console.log(`User ${name} has joined room ${chatRooms[0]}`);
-  // io.emit('Welcome Message', name);
-  // });
-
-  // General send message
   socket.on('chat message', (data) => {
     io.emit('chat message', {
       username: socket.userName,
       message: data,
       timestamp: new Date().toLocaleTimeString(),
     });
-    // io.in(chatRooms[0]).emit('chat message', msg);
-
-    // console.log(`User ${socket.userName} wrote ${data}`);
-    // console.log(users);
   });
 
   // forward the private message to the right recipient
@@ -83,7 +57,6 @@ io.on('connection', (socket) => {
     });
   });
 
-  // notify users upon disconnection
   /// SERVER SIDE
   socket.on('disconnect', () => {
     socket.emit('user disconnected', socket.id); //
@@ -95,7 +68,6 @@ io.on('connection', (socket) => {
       });
     }
     io.emit('users', users);
-    // socket.leave(chatRooms[0]);
     console.log('user disconnected', socket.id);
   });
 });
