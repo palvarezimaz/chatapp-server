8544f2eb286bfc9e7635ef24feb30e1db2a5c42d
Pablo <palvarezimaz@gmail.com>
2025-03-21 17:59:48 -0300

    more cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 let origin = 'http://localhost:3000';
 
 if (process.env.NODE_ENV === 'production') {
-  origin = 'https://palvarezimaz.github.io/';
+  origin = 'https://palvarezimaz.github.io/chatapp-client';
 }
 const io = socketIo(server, {
   cors: {
